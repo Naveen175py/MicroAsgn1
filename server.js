2c2d44d410a67baec7f8d440deb0ab1eeacf8cac
code naveen <iitiannaveen175@gmail.com>
2022-12-22 22:06:10 +0530

    added database models

--- server.js
+++ server.js
@@ -7,25 +7,8 @@ const initializeDatabase = require("./config/db");
 app.get("/api/health",(req,res)=>{
     res.send(`backend server is up and running:${new Date}`)
 })
-const mongoose = require("mongoose");
-const mongoUrl = process.env.MONGODB_URI;
 
-function databaseConnection() {
-    mongoose.set("strictQuery", true);
-    mongoose
-        .connect(mongoUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("Database Connected Successfully");
-        })
-        .catch((err) => {
-            console.log("Database Not Connected Successfully : " + err);
-        });
-}
 
-module.exports = databaseConnection;
 const port=process.env.PORT||3000;
 const host=process.env.HOST||'localhost';
 
