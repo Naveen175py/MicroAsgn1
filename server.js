e34a1754759bb1cc8b9aa8efd4961303208a64c6
code naveen <iitiannaveen175@gmail.com>
2022-12-26 13:38:58 +0530

    added create a category api

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const express=require("express");
+const express = require("express");
 
-const app=express();
+const app = express();
 const initializeDatabase = require("./config/db");
-app.get("/api/health",(req,res)=>{
-    res.send(`backend server is up and running:${new Date}`)
-})
-
-
-const port=process.env.PORT||3000;
-const host=process.env.HOST||'localhost';
-
-app.listen(port,()=>{
-    console.log(`Express server is listening at https://${host}:${port}`) 
+app.get("/api/health", (req, res) => {
+  res.send(`backend server is up and running:${new Date()}`);
 });
 
+app.use("/api/admin", admin);
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
+app.listen(port, () => {
+  console.log(`Express server is listening at https://${host}:${port}`);
+});
