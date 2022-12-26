c29569ac57dab96f64552542bb2444e6bf5b180e
code naveen <iitiannaveen175@gmail.com>
2022-12-26 15:32:06 +0530

    added save image as favourite api

--- routes/discover.js
+++ routes/discover.js
@@ -3,6 +3,37 @@ const route = Router();
 const CategoryModel = require("../models/category");
 const GalleryModel = require("../models/gallery");
 
+route.get("/like/:imageId", async (req, res, next) => {
+  try {
+    const imageId = req.params.imageId;
+    if (!imageId) {
+      res.status(400).send("Bad Request");
+    }
+
+    let likeValue;
+
+    const imageDetails = await GalleryModel.findOne({ _id: imageId });
+
+    if (imageDetails) {
+      if (imageDetails.likes) {
+        likeValue = 0;
+      } else {
+        likeValue = 1;
+      }
+    }
+
+    await GalleryModel.updateOne(
+      { _id: imageId },
+      { $set: { likes: likeValue } }
+    );
+
+    res.send("Favorite updated successfully");
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 route.get("/discover/:category", async (req, res, next) => {
   try {
     const category = req.params.category;
@@ -34,7 +65,9 @@ route.get("/discover/:category", async (req, res, next) => {
       category: { $in: [category] },
       ...filter,
     })
-      .sort({ createdAt: sort }).skip(skip).limit(4);
+      .sort({ createdAt: sort })
+      .skip(skip)
+      .limit(4);
 
     res.json(galleryDetails);
   } catch (error) {
